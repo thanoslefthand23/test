c89c6b44f4e32b8f2283eb9e6bd26bf3a5adea45
Harsh Tailor <tailorha@sheridancollege.ca>
2023-06-19 18:58:32 -0400

    testing 3

---
+++ src/studentlist/Studentlist1.java
@@ -0,0 +1,35 @@
+package studentlist1;
+/* 
+   @author tailorha
+   @author harsh tailor 29th may 2023
+   testing
+*/
+public class Student {
+
+ 
+    
+    private String sname;
+    private String sID;
+    
+    public Student(String sname, String sID){
+        this.sname = sname;
+        this.sID = sID;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getsID() {
+        return sID;
+    }
+
+    public void setsID(String sID) {
+        this.sID = sID;
+    }
+    
+}
